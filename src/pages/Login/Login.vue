892776639c4480888409715dc098073e0bf91d24
xtomoya <40332021+xtomoya@users.noreply.github.com>
2020-03-20 15:02:39 +0800

    做到了前端验证

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -4,39 +4,42 @@
             <div class="login_header">
                 <h2 class="login_logo">硅谷外卖</h2>
                 <div class="login_header_title">
-                    <a href="javascript:;" class="on">短信登录</a>
-                    <a href="javascript:;">密码登录</a>
+                    <a href="javascript:;" :class="{on:loginWay}" @click="loginWay=true">短信登录</a>
+                    <a href="javascript:;" :class="{on:!loginWay}" @click="loginWay=false">密码登录</a>
                 </div>
             </div>
             <div class="login_content">
-                <form>
-                    <div class="on">
+                <form @submit.prevent="login">
+                    <div :class="{on:loginWay}">
                         <section class="login_message">
-                            <input type="tel" maxlength="11" placeholder="手机号">
-                            <button disabled="disabled" class="get_verification">获取验证码</button>
+                            <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
+                            <button :disabled="!rightPhone" class="get_verification" @click.prevent="getCode" :class="{right_phone:rightPhone}">
+                                {{computeTime>0?'已发送（'+computeTime+'）s':'获取验证码'}}
+                            </button>
                         </section>
                         <section class="login_verification">
-                            <input type="tel" maxlength="8" placeholder="验证码">
+                            <input type="tel" maxlength="8" placeholder="验证码" v-model="code">
                         </section>
                         <section class="login_hint">
                             温馨提示：未注册硅谷外卖帐号的手机号，登录时将自动注册，且代表已同意
                             <a href="javascript:;">《用户服务协议》</a>
                         </section>
                     </div>
-                    <div>
+                    <div :class="{on:!loginWay}">
                         <section>
                             <section class="login_message">
-                                <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
+                                <input type="text" maxlength="11" placeholder="手机/邮箱/用户名" v-model="name">
                             </section>
                             <section class="login_verification">
-                                <input type="tel" maxlength="8" placeholder="密码">
-                                <div class="switch_button off">
-                                    <div class="switch_circle"></div>
-                                    <span class="switch_text">...</span>
+                                <input type="text" maxlength="8" placeholder="密码" v-if="showPwd" v-model="pwd">
+                                <input type="password" maxlength="8" placeholder="密码" v-else v-model="pwd">
+                                <div class="switch_button" :class="showPwd?'on':'off'" @click="showPwd=!showPwd">
+                                    <div class="switch_circle" :class="{right:showPwd}"></div>
+                                    <span class="switch_text">{{showPwd?'abc':''}}</span>
                                 </div>
                             </section>
                             <section class="login_message">
-                                <input type="text" maxlength="11" placeholder="验证码">
+                                <input type="text" maxlength="11" placeholder="验证码" v-model="captcha">
                                 <img class="get_verification" src="./images/captcha.svg" alt="captcha">
                             </section>
                         </section>
@@ -49,11 +52,87 @@
                 <i class="iconfont iconreturn"></i>
             </a>
         </div>
+        <AlertTip :alertText="alertText" v-show="showAlert" @closeTip="closeTip"></AlertTip>
     </section>
+
 </template>
 
 <script>
-    export default {}
+    import AlertTip from '../../components/AlertTip/AlertTip'
+    export default {
+        data(){
+          return{
+              loginWay:true, //为true时代表短信登陆，false时代表密码登陆
+              phone:'', //手机号
+              computeTime:0,//计时时间
+              showPwd:false,//是否显示密码
+              pwd:'',//密码
+              code:'',//短信验证码
+              name:'',//帐号名称
+              captcha:'',//图片验证码
+              alertText:'',//弹出的提示框中的文本
+              showAlert:false,  //是否显示提示框
+          }
+        },
+        components:{
+            AlertTip,
+        },
+        computed:{
+            rightPhone(){
+                return /^1\d{10}$/.test(this.phone)
+            },
+        },
+        methods:{
+            //异步获取手机验证码
+            getCode(){
+                //启动倒计时
+                if (!this.computeTime){
+                    this.computeTime = 30
+                    const intervalId = setInterval(()=>{
+                        this.computeTime--
+                        if (this.computeTime<=0){
+                            clearInterval(intervalId)
+                        }
+                    },1000)
+                    //发送ajax请求，要求服务器向指定的手机号码发送验证码
+                }
+
+            },
+            showAlert1(text){
+                this.showAlert = true
+                this.alertText = text
+            },
+            //异步登陆
+            login(){
+                //表单数据的验证，前台验证
+                if (this.loginWay){ //短信登陆
+                    // const {rightPhone,phone,code} = this
+                    if (!this.phone){
+                        //手机号码不正确
+                        this.showAlert1('手机号不正确')
+                    }else if (!this.rightPhone){
+                        this.showAlert1('手机号不正确')
+                    } else if (!/^\d{6}$/.test(this.code)){
+                        //验证必须是6位数字
+                        this.showAlert1('验证码不正确')
+                    }
+                } else {  //密码登陆
+                    // const {name,pwd,captcha} = this
+                    if (!this.name){
+                        this.showAlert1('用户名必须填')
+                    } else if (!this.pwd){
+                        this.showAlert1('密码必须填')
+                    } else if (!this.captcha){
+                        this.showAlert1('验证码必须填')
+                    }
+                }
+            },
+            closeTip(){
+                this.showAlert = false
+                this.alertText = ''
+            },
+        },
+    }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
@@ -117,6 +196,8 @@
                                 color #ccc
                                 font-size 14px
                                 background transparent
+                                &.right_phone
+                                    color black
                         .login_verification
                             position relative
                             margin-top 16px
@@ -156,6 +237,8 @@
                                     background #fff
                                     box-shadow 0 2px 4px 0 rgba(0,0,0,.1)
                                     transition transform .3s
+                                    &.right
+                                        transform translateX(30px)
                         .login_hint
                             margin-top 12px
                             color #999
--- src/pages/MSite/MSite.vue
+++ src/pages/MSite/MSite.vue
@@ -1,18 +1,5 @@
 <template>
     <section class="msite">
-        <!--首页头部-->
-        <!--<header class="header">-->
-          <!--<span class="header_search">-->
-            <!--<i class="iconfont icon-sousuo"></i>-->
-          <!--</span>-->
-            <!--<span class="header_title">-->
-            <!--<span class="header_title_text ellipsis">昌平区北七家宏福科技园(337省道北)</span>-->
-          <!--</span>-->
-            <!--<span class="header_login">-->
-            <!--<span class="header_login_text">登录|注册</span>-->
-          <!--</span>-->
-        <!--</header>-->
-
         <HeaderTop :title="address.name">
             <span class="header_search" slot="left">
                 <i class="iconfont icon-sousuo"></i>
@@ -23,7 +10,7 @@
         </HeaderTop>
         <!--首页导航-->
         <nav class="msite_nav">
-            <div class="swiper-container">
+            <div class="swiper-container" v-if="categorys.length">
                 <div class="swiper-wrapper">
                     <div class="swiper-slide" v-for="(categorys,index) in categorysArr" :key="index">
                         <a href="javascript:" class="link_to_food" v-for="(category,index) in categorys" :key="index">
@@ -34,60 +21,11 @@
                         </a>
 
                     </div>
-                    <!--<div class="swiper-slide">-->
-                        <!--<a href="javascript:" class="link_to_food">-->
-                            <!--<div class="food_container">-->
-                                <!--<img src="./images/nav/9.jpg">-->
-                            <!--</div>-->
-                            <!--<span>甜品饮品</span>-->
-                        <!--</a>-->
-                        <!--<a href="javascript:" class="link_to_food">-->
-                            <!--<div class="food_container">-->
-                                <!--<img src="./images/nav/10.jpg">-->
-                            <!--</div>-->
-                            <!--<span>商超便利</span>-->
-                        <!--</a>-->
-                        <!--<a href="javascript:" class="link_to_food">-->
-                            <!--<div class="food_container">-->
-                                <!--<img src="./images/nav/11.jpg">-->
-                            <!--</div>-->
-                            <!--<span>美食</span>-->
-                        <!--</a>-->
-                        <!--<a href="javascript:" class="link_to_food">-->
-                            <!--<div class="food_container">-->
-                                <!--<img src="./images/nav/12.jpg">-->
-                            <!--</div>-->
-                            <!--<span>简餐</span>-->
-                        <!--</a>-->
-                        <!--<a href="javascript:" class="link_to_food">-->
-                            <!--<div class="food_container">-->
-                                <!--<img src="./images/nav/13.jpg">-->
-                            <!--</div>-->
-                            <!--<span>新店特惠</span>-->
-                        <!--</a>-->
-                        <!--<a href="javascript:" class="link_to_food">-->
-                            <!--<div class="food_container">-->
-                                <!--<img src="./images/nav/14.jpg">-->
-                            <!--</div>-->
-                            <!--<span>准时达</span>-->
-                        <!--</a>-->
-                        <!--<a href="javascript:" class="link_to_food">-->
-                            <!--<div class="food_container">-->
-                                <!--<img src="./images/nav/1.jpg">-->
-                            <!--</div>-->
-                            <!--<span>预订早餐</span>-->
-                        <!--</a>-->
-                        <!--<a href="javascript:" class="link_to_food">-->
-                            <!--<div class="food_container">-->
-                                <!--<img src="./images/nav/2.jpg">-->
-                            <!--</div>-->
-                            <!--<span>土豪推荐</span>-->
-                        <!--</a>-->
-                    <!--</div>-->
                 </div>
                 <!-- Add Pagination -->
                 <div class="swiper-pagination"></div>
             </div>
+            <img src="./images/msite_back.svg" alt="back" v-else>
         </nav>
         <!--首页附近商家-->
         <div class="msite_shop_list">
